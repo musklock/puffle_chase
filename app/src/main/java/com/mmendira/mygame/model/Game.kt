1c906bef2d1fdcbca7c7ecee76ee0ad382b12913
muskaan <mmendira@u.rochester.edu>
2020-03-28 17:34:04 -0400

    game boards made for easy, medium and difficult

--- app/src/main/java/com/mmendira/mygame/model/Game.kt
+++ app/src/main/java/com/mmendira/mygame/model/Game.kt
@@ -1,15 +1,62 @@
 package com.mmendira.mygame.model
 
+import com.mmendira.mygame.R
+
+class Location(tile: Game.Tile){
+    var tile = tile
+    var isNext = false
+    fun isValid():Boolean{
+        if (tile == Game.Tile.GOOD && isNext){
+            return true
+        }
+        return false
+    }
+    fun lostGame(): Boolean{
+        if (isNext && tile == Game.Tile.BAD){
+            return true
+        }
+        return false
+    }
+
+}
+
 class Game {
 
     val levelsList = listOf("Easy", "Medium", "Hard")
     val levelsMap = mapOf("Easy" to "Easy",
                             "Medium" to "Medium",
                             "Hard" to "Hard")
+    var level: String
+    var mode:  String
+
+
+    enum class Tile{
+        GOOD, BAD, WINNING, EMPTY, PUFFLE
+    }
+
+    init{
+        mode = "fire"
+        level = "Hard"
+    }
 
     fun levelSelected(level: String): String?{
         return levelsMap.getValue(level)
+    }
 
+    fun getImageId(tile: Tile) = when (tile) {
+        Tile.GOOD -> R.drawable.tile
+        Tile.WINNING -> R.drawable.winningtile
+        Tile.EMPTY -> 0
+        Tile.BAD -> fireOrIce()
+        Tile.PUFFLE -> R.drawable.puffle
+    }
+
+    private fun fireOrIce(): Int{
+        if (mode == "fire"){
+            return R.drawable.fire
+        }else{
+            return R.drawable.iceblock
+        }
     }
 
 }
--- app/src/main/java/com/mmendira/mygame/ui/main/game/GameFragment.kt
+++ app/src/main/java/com/mmendira/mygame/ui/main/game/GameFragment.kt
@@ -9,9 +9,63 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 
 import com.mmendira.mygame.R
+import com.mmendira.mygame.model.Game
+import com.mmendira.mygame.model.Location
 import kotlinx.android.synthetic.main.game_fragment.*
 
 class GameFragment : Fragment() {
+    var game = Game()
+    private lateinit var location_1: Location
+    private lateinit var location_2: Location
+    private lateinit var location_3: Location
+    private lateinit var location_4: Location
+    private lateinit var location_5: Location
+    private lateinit var location_6: Location
+    private lateinit var location_7: Location
+    private lateinit var location_8: Location
+    private lateinit var location_9: Location
+    private lateinit var location_10: Location
+
+
+    fun initiateLocations(){
+        if (game.level == "Easy"){
+            location_1 = Location(Game.Tile.PUFFLE)
+            location_2 = Location(Game.Tile.GOOD)
+            location_3 = Location(Game.Tile.GOOD)
+            location_4 = Location(Game.Tile.WINNING)
+            location_5 = Location(Game.Tile.EMPTY)
+            location_6 = Location(Game.Tile.EMPTY)
+            location_7 = Location(Game.Tile.EMPTY)
+            location_8 = Location(Game.Tile.EMPTY)
+            location_9 = Location(Game.Tile.EMPTY)
+            location_10 = Location(Game.Tile.EMPTY)
+
+        }else if (game.level == "Medium"){
+            location_1 = Location(Game.Tile.PUFFLE)
+            location_2 = Location(Game.Tile.BAD)
+            location_7 = Location(Game.Tile.GOOD)
+            location_8 = Location(Game.Tile.GOOD)
+            location_9 = Location(Game.Tile.GOOD)
+            location_3 = Location(Game.Tile.GOOD)
+            location_4 = Location(Game.Tile.WINNING)
+            location_5 = Location(Game.Tile.EMPTY)
+            location_6 = Location(Game.Tile.EMPTY)
+            location_10 = Location(Game.Tile.EMPTY)
+
+        }else if (game.level == "Hard"){
+            location_5 = Location(Game.Tile.BAD)
+            location_6 = Location(Game.Tile.GOOD)
+            location_7 = Location(Game.Tile.BAD)
+            location_2 = Location(Game.Tile.GOOD)
+            location_8 = Location(Game.Tile.GOOD)
+            location_9 = Location(Game.Tile.GOOD)
+            location_3 = Location(Game.Tile.BAD)
+            location_10 = Location(Game.Tile.GOOD)
+            location_1 = Location(Game.Tile.PUFFLE)
+            location_4 = Location(Game.Tile.WINNING)
+        }
+
+    }
 
     companion object {
         fun newInstance() = GameFragment()
@@ -29,10 +83,65 @@ class GameFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+        initiateLocations()
+        makeBoard()
         game_over.setOnClickListener {
             view.findNavController().navigate(R.id.action_gameFragment_to_gameOverFragment)
             viewModel.transitionSound()
         }
+        location1.setOnClickListener{
+            viewModel.playSound()
+
+        }
+        location2.setOnClickListener{
+            viewModel.playSound()
+        }
+        location3.setOnClickListener{
+            viewModel.playSound()
+        }
+        location4.setOnClickListener{
+            location3.setImageResource(R.drawable.fire)
+            view.findNavController().navigate(R.id.action_gameFragment_to_gameOverFragment)
+            viewModel.transitionSound()
+        }
+        location3.setOnClickListener{
+            viewModel.playSound()
+        }
+        location3.setOnClickListener{
+            viewModel.playSound()
+        }
+        location3.setOnClickListener{
+            viewModel.playSound()
+        }
+        location3.setOnClickListener{
+            viewModel.playSound()
+        }
+        location3.setOnClickListener{
+
+        }
+        location3.setOnClickListener{
+
+        }
+        location3.setOnClickListener{
+
+        }
+        location3.setOnClickListener{
+
+        }
+    }
+
+    private fun makeBoard(){
+        location1.setImageResource(game.getImageId(location_1.tile))
+        location2.setImageResource(game.getImageId(location_2.tile))
+        location3.setImageResource(game.getImageId(location_3.tile))
+        location4.setImageResource(game.getImageId(location_4.tile))
+        location5.setImageResource(game.getImageId(location_5.tile))
+        location6.setImageResource(game.getImageId(location_6.tile))
+        location7.setImageResource(game.getImageId(location_7.tile))
+        location8.setImageResource(game.getImageId(location_8.tile))
+        location9.setImageResource(game.getImageId(location_9.tile))
+        location10.setImageResource(game.getImageId(location_10.tile))
+
     }
 
 }
