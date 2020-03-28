9119b9e60be23bb35a7b768d810759d26fa75001
muskaan <mmendira@u.rochester.edu>
2020-03-27 21:21:52 -0400

    choose a level screen done and lookin cute

--- app/src/main/java/com/mmendira/mygame/model/Game.kt
+++ app/src/main/java/com/mmendira/mygame/model/Game.kt
@@ -3,9 +3,9 @@ package com.mmendira.mygame.model
 class Game {
 
     val levelsList = listOf("Easy", "Medium", "Hard")
-    val levelsMap = mapOf("Easy" to "Easy level",
-                            "Medium" to "Medium level",
-                            "Hard" to "Hard level")
+    val levelsMap = mapOf("Easy" to "Easy",
+                            "Medium" to "Medium",
+                            "Hard" to "Hard")
 
     fun levelSelected(level: String): String?{
         return levelsMap.getValue(level)
